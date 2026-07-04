a8efeee419ee92d489e94b49072367c0ac41d03b
hw-lunemann <hw.lunemann@gmail.com>
2026-07-04 21:01:44 +0200

    protocol: implement send and receive

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,6 +1,6 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
-use serde::{Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Serialize};
 
 type User = String;
 
@@ -42,9 +42,28 @@ enum ClientResponse {
 }
 
 fn send<M: Serialize>(stream: &mut TcpStream, message: &M) -> std::io::Result<()> {
-        
+    let data = serde_json::to_vec(message)
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+    let len = (data.len() as u32).to_be_bytes();
+
+    stream.write_all(&len)?;
+    stream.write_all(&data)?;
+
+    Ok(())
+
 }
 
-fn receive<M: Deserialize>(stream: &mut TcpStream) -> std::io::Result<M> {
-        
+fn receive<M: DeserializeOwned>(stream: &mut TcpStream) -> std::io::Result<M> {
+    let mut len_buf = [0u8; 4];
+    stream.read_exact(&mut len_buf)?;
+    let len = u32::from_be_bytes(len_buf) as usize;
+
+    let mut buf = vec![0; len];
+    stream.read_exact(&mut buf)?;
+
+    let msg = serde_json::from_slice(&buf)
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+    Ok(msg)
 }
