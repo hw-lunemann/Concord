3bf5d2d761850721fe4cd5e50c15e59d8ca91d99
hw-lunemann <hw.lunemann@gmail.com>
2026-07-05 18:30:05 +0200

    protocol: fix visibility

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,49 +1,48 @@
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
 use std::io::prelude::*;
 use std::net::TcpStream;
-use serde::{de::DeserializeOwned, Serialize};
 
-type User = String;
-const server_port: i32 = 15555;
+pub const server_port: i32 = 15555;
+pub type User = String;
 
 #[derive(Serialize, Deserialize, Debug)]
-enum ClientCommand {
+pub enum ClientCommand {
     // Username, Public Key
     LogIn(String, String),
     // Message vom client zum server
     SendMessage(String),
     // TODO
     StartVoiceCall(),
-    StartVideoCall()
+    StartVideoCall(),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-enum ServerResponse {
+pub enum ServerResponse {
     // Okay
     Ack(ClientCommand),
     // Command, Reason for error
-    Error(ClientCommand, String)
+    Error(ClientCommand, String),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-enum ServerCommand {
+pub enum ServerCommand {
     // Message vom server zum client
     SendMessage(String),
     // Connect new user
     UserConnected(User),
     // Disconnect user
-    UserDisconnected(User)
+    UserDisconnected(User),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-enum ClientResponse {
+pub enum ClientResponse {
     // Okay
     Ack(ServerCommand),
     // Command, Reason for error
-    Error(ServerCommand, String)
+    Error(ServerCommand, String),
 }
 
-fn send<M: Serialize>(stream: &mut TcpStream, message: &M) -> std::io::Result<()> {
+pub fn send<M: Serialize>(stream: &mut TcpStream, message: &M) -> std::io::Result<()> {
     let data = serde_json::to_vec(message)
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
 
@@ -53,10 +52,9 @@ fn send<M: Serialize>(stream: &mut TcpStream, message: &M) -> std::io::Result<()
     stream.write_all(&data)?;
 
     Ok(())
-
 }
 
-fn receive<M: DeserializeOwned>(stream: &mut TcpStream) -> std::io::Result<M> {
+pub fn receive<M: DeserializeOwned>(stream: &mut TcpStream) -> std::io::Result<M> {
     let mut len_buf = [0u8; 4];
     stream.read_exact(&mut len_buf)?;
     let len = u32::from_be_bytes(len_buf) as usize;
